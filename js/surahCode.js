91885614e276eed58bf0021d650f592c279db82c
abdul380 <72687514+abdul380@users.noreply.github.com>
2020-10-28 13:55:00 +0800

    Update surahCode.js

--- js/surahCode.js
+++ js/surahCode.js
@@ -1,9 +1,5 @@
 for (var i=0;i<114;i++)
     {
-    document.getElementById("surah").innerHTML += "<a href=\"" + (i+1) + ".html\" class=\"button skyBlueButton\">" + (i + 1) + ". " +  surah[i].englishTransliteration + " (" + surah[i].ayat +" verses)</a>";
+    document.getElementById("surah").innerHTML += "<a href=\"" + (i+1) + ".html\" class=\"button skyBlueButton\">" + (i + 1) + ". " +  surah[i].englishTransliteration + " " + surah[i].englishTranslation + " (" + surah[i].ayat +" verses)</a>";
     } 
-    
-function changeMaximumAyat()
-{
-document.getElementById("ayat#").max = surah[(document.getElementById("surah#").value-1)].ayat;
-}
+  
